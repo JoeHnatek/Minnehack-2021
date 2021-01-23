cfce7a08b289e2b7996409d5d82717138b7624c5
Joseph Hnatek <24803352+jmh07@users.noreply.github.com>
2021-01-23 15:51:56 -0600

    Added general api points and we are now serving our frontend

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../frontend/greenship/build/static', template_folder='../frontend/greenship/build')
 
 @app.route('/')
 def index():
-    return "wow"
+    return render_template("index.html")
+
+@app.route('/scrape/<tracking>/', methods=["POST", "GET"])  # GET is only there as a test
+def scrape(tracking):
+    return "scraping {}...".format(tracking)
+
+@app.route('/info/<tracking>/', methods=["GET"])
+def getInfo(tracking):
+    return "getting {} info...".format(tracking)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
